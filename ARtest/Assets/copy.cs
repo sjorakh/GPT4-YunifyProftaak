ac7b36e7bf1af31d6d627333067f7f8fd80826ac
Sjorakh <355792@fhict.fontys.nl>
2018-05-18 14:54:12 +0200

    instantiate on recognision something else

---
+++ ARtest/Assets/copy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class copy : MonoBehaviour {
+
+	bool exists = false;
+	public GameObject kurwa;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!exists && gameObject.GetComponent<MeshRenderer>().isVisible) {
+			GameObject kurwa2 = Instantiate (kurwa);
+			kurwa2.transform.position = gameObject.transform.parent.transform.position;
+			exists = true;
+		}
+	}
+}
